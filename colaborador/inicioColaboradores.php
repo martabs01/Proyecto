de182bab594195faafca96d041a8feda4eadae35
martabs01 <77440756+martabs01@users.noreply.github.com>
2021-12-07 20:37:04 +0100

    Delete colaborador directory

--- colaborador/inicioColaboradores.php
+++
@@ -1,9 +0,0 @@
-<!--
-    Alumno: Marta Broncano Suárez
-    Asignatura: Proyecto San Romilla
-    Curso: 20-21
-    Descripción: Página inicial de la gestión de los coolaboradores
--->
-<?php
-    echo'BIENBENIDO A LA PAGINA DE INICIO DE COLABORADORES';
-?>
